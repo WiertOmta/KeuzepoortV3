f1414f45f5d2b77567373e2c6c51c3955f3f292b
Pim te Slaa <pimteslaa@gmail.com>
2015-02-22 15:25:59 +0100

    New listener pattern implemented

--- Keuzepoort/src/FalseSensor.java
+++ Keuzepoort/src/FalseSensor.java
@@ -1,8 +1,10 @@
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.PinPullResistance;
 
 public class FalseSensor extends Sensor {
 
