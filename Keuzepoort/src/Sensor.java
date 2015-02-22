bfb8793f645b09abe9790ce21e6b9a1f52c13af9
Pim te Slaa <pimteslaa@gmail.com>
2015-02-22 15:53:47 +0100

    Fixed a bug with the GpioFactory

--- Keuzepoort/src/Sensor.java
+++ Keuzepoort/src/Sensor.java
@@ -3,6 +3,6 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 
 public abstract class Sensor extends Thread {
-	protected final GpioController gpioController = GpioFactory.GetInstance();
+	protected final GpioController gpioController = GpioFactory.getInstance();
 	protected GpioPinDigitalInput pin;
 }
